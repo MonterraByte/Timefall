d2811b8cf544fe6636ec7f47d361e53efdc2badb
AJRC2020 <72152717+AJRC2020@users.noreply.github.com>
2023-04-25 15:10:50 +0100

    Melee combat basics and dummy created

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,20 +6,28 @@ public class PlayerScript : MonoBehaviour {
     public float playerAcceleration = 1.0f;
     public float jumpHeight = 1.0f;
     public float gravityValue = -9.81f;
+    public float dashSpeed = 5.0f;
 
     private CharacterController characterController;
+    private BoxCollider stickCollider;
 
     private Vector2 moveInput;
     private float jumpInput;
     private const float jumpInputMax = 0.1f;
+    private bool meleeInput;
 
     private float coyoteTime;
     private const float coyoteTimeLength = 0.15f;
     private bool isGrounded;
+    private bool isRight = true;
     private Vector3 playerVelocity;
 
     private void Start() {
         characterController = GetComponent<CharacterController>();
+
+        this.stickCollider = this.transform.GetChild(1).gameObject.GetComponent<BoxCollider>();
+
+        this.stickCollider.enabled = false;
     }
 
     private bool CanJump() {
@@ -36,6 +44,13 @@ public class PlayerScript : MonoBehaviour {
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnMelee(InputAction.CallbackContext context){
+        if (context.started) {
+            meleeInput = true;
+            this.stickCollider.enabled = true;
+        }
+    }
+
     private void Update() {
         isGrounded = characterController.isGrounded;
         var velocity = characterController.velocity;
@@ -49,6 +64,20 @@ public class PlayerScript : MonoBehaviour {
             }
         }
 
+        if (meleeInput){
+            if (!isGrounded) {
+                //attack air
+            }
+            if (velocity.x != 0.0f) {
+                velocity.x *= dashSpeed;
+            }
+            else {
+                //normal punch
+            }
+            meleeInput = false;
+            //this.stickCollider.enabled = false;
+        }
+
         if (jumpInput > 0.0f && CanJump()) {
             coyoteTime = 0.0f;
             velocity.y = Mathf.Sqrt(jumpHeight * 2.0f * -gravityValue);
@@ -58,6 +87,16 @@ public class PlayerScript : MonoBehaviour {
 
         velocity.x = Mathf.Lerp(velocity.x, moveInput.x * playerSpeed, playerAcceleration * Time.deltaTime);
         velocity.y += gravityValue * Time.deltaTime;
+
+        if (isRight && velocity.x < 0.0f) {
+            this.transform.Rotate(0.0f, 180.0f, 0.0f);
+            isRight = false;
+        }
+
+        if (!isRight && velocity.x > 0.0f) {
+            this.transform.Rotate(0.0f, 180.0f, 0.0f);
+            isRight = true;
+        }
 
         characterController.Move(velocity * Time.deltaTime);
     }
