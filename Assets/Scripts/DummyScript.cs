bb677bddec2ff26118520589444929c55cf14873
AJRC2020 <72152717+AJRC2020@users.noreply.github.com>
2023-05-04 19:00:18 +0100

    Target now gets phased when hit

--- Assets/Scripts/DummyScript.cs
+++ Assets/Scripts/DummyScript.cs
@@ -11,37 +11,40 @@ public class DummyScript : MonoBehaviour {
     private float hitTime;
     private float currentTime;
     private bool hitRight;
+    private float hitDistance;
 
     // Start is called before the first frame update
     void Start() {
-        this.boxCollider = GetComponent<BoxCollider>(); 
+        this.boxCollider = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update(){
         if (this.isHit && this.hitTime > currentTime) {
             this.currentTime += Time.deltaTime;
-            if (hitRight) this.transform.position += Vector3.left * 10 * Time.deltaTime;
-            else this.transform.position += Vector3.right * 10 * Time.deltaTime;
+            if (hitRight) this.transform.position += Vector3.left * this.hitDistance * Time.deltaTime;
+            else this.transform.position += Vector3.right * this.hitDistance * Time.deltaTime;
         }
         else if (this.isHit) {
             this.isHit = false;
+            this.boxCollider.enabled = true;
         }
-
-        this.transform.position += Vector3.right * Time.deltaTime;
-        this.boxCollider.enabled = true;
+        else
+        {
+            this.transform.position += Vector3.right * Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.layer == 1 && !this.isHit) {
-            GotHit(other);
+            GotHit(other.transform, 0);
         }  
     }
 
     private void OnTriggerStay(Collider other) {
         if (other.gameObject.layer == 1 && !this.isHit)
         {
-            GotHit(other);
+            GotHit(other.transform, 0);
         }
     }
 
@@ -49,7 +52,7 @@ public class DummyScript : MonoBehaviour {
     {
         if (collision.gameObject.layer == 2 && !this.isHit)
         {
-            GotHit(collision);
+            GotHit(collision.transform, 10);
         }
     }
 
@@ -57,16 +60,18 @@ public class DummyScript : MonoBehaviour {
     {
         if (collision.gameObject.layer == 2 && !this.isHit)
         {
-            GotHit(collision);
+            GotHit(collision.transform, 10);
         }
     }
 
-    private void GotHit(Collision collision)
+    private void GotHit(Transform collisionTransform, float distance)
     {
         this.currentTime = 0.0f;
         this.hitTime = 0.35f;
         this.isHit = true; ;
-        if (collision.transform.position.x > transform.position.x) this.hitRight = true;
+        this.hitDistance = distance;
+        this.boxCollider.enabled = false;
+        if (collisionTransform.position.x > transform.position.x) this.hitRight = true;
         else this.hitRight = false;
-    } 
+    }
 }
