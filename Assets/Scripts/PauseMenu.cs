7a859fd0f2760ebdbaec0ed3c2c29e61408c70c1
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2023-06-20 16:39:45 +0100

    Allow using the pause menu through the keyboard or the controller

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,9 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
     private bool _isPaused;
@@ -14,32 +17,57 @@ public class PauseMenu : MonoBehaviour {
     }
 
     public GameObject pauseMenuUI;
+    public Button[] buttons;
     public InputActionReference pauseAction;
+    public InputActionReference upAction;
+    public InputActionReference downAction;
+    public InputActionReference confirmAction;
+
+    private int _selectedButton;
+    private int SelectedButton {
+        get => _selectedButton;
+        set {
+            _selectedButton = Math.Clamp(value, 0, buttons.Length - 1);
+            EventSystem.current.SetSelectedGameObject(buttons[_selectedButton].gameObject);
+        }
+    }
 
     private void Start() {
-        pauseAction.action.started += ctx => {
-            if (ctx.started) {
-                IsPaused = !IsPaused;
+        pauseAction.action.started += _ => { IsPaused = !IsPaused; };
+        upAction.action.started += _ => {
+            if (IsPaused) {
+                SelectedButton--;
+            }
+        };
+        downAction.action.started += _ => {
+            if (IsPaused) {
+                SelectedButton++;
+            }
+        };
+        confirmAction.action.started += _ => {
+            if (IsPaused) {
+                buttons[SelectedButton].onClick.Invoke();
             }
         };
     }
 
     private void OnEnable() {
         pauseAction.action.Enable();
+        upAction.action.Enable();
+        downAction.action.Enable();
+        confirmAction.action.Enable();
     }
 
     private void OnDisable() {
         pauseAction.action.Disable();
     }
 
-    public void LoadMenu()
-    {
+    public void LoadMenu() {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void QuitGame()
-    {
+    public void QuitGame() {
         Debug.Log("Quitting game...");
         Application.Quit();
     }
