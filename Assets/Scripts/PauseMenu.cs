cbf5860120cfa17a8cc1a352fe05259332548ac0
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2023-06-20 15:13:14 +0100

    Convert the pause menu to use input actions
    Now you can pause with a controller

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,45 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 
-public class PauseMenu : MonoBehaviour
-{
-    public static bool GameIsPaused = false;
-
-    public GameObject pauseMenuUI;
-
-    void Update()
-    {
-        if (Keyboard.current.escapeKey.wasPressedThisFrame)
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
+public class PauseMenu : MonoBehaviour {
+    private bool _isPaused;
+    public bool IsPaused {
+        get => _isPaused;
+        set {
+            _isPaused = value;
+            pauseMenuUI.SetActive(_isPaused);
+            Time.timeScale = _isPaused ? 0.0f : 1.0f;
         }
     }
 
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public InputActionReference pauseAction;
+
+    private void Start() {
+        pauseAction.action.started += ctx => {
+            if (ctx.started) {
+                IsPaused = !IsPaused;
+            }
+        };
     }
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
+
+    private void OnEnable() {
+        pauseAction.action.Enable();
+    }
+
+    private void OnDisable() {
+        pauseAction.action.Disable();
     }
 
     public void LoadMenu()
-
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
