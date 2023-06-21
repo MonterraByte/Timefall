3bfc75cd460e76a5ea0ba2fc9a02ead01c80109e
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2023-06-21 18:00:18 +0100

    Adjust spawn point in boss arena, and integrate the door closing script

--- Assets/Scripts/BossArenaScript.cs
+++ Assets/Scripts/BossArenaScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -5,24 +6,50 @@ public class BossArenaScript : MonoBehaviour {
     public GameObject boss;
     public CinemachineCamera defaultCamera;
     public CinemachineCamera bossFightCamera;
+    public GameObject horizontalDoor;
+    public GameObject respawnPoint;
 
     private Boss bossScript;
+    private Vector3 horizontalDoorInitialPosition;
 
     void Start() {
         bossScript = boss.GetComponent<Boss>();
         bossScript.enabled = false;
+        horizontalDoorInitialPosition = horizontalDoor.transform.position;
     }
 
     private void OnTriggerEnter(Collider other) {
-        bossScript.enabled = true;
+        if (other.gameObject.GetComponent<PlayerScript>() == null) {
+            return;
+        }
+
+        var respawnManager = FindObjectOfType<RespawnManagerScript>();
+        respawnManager.setSpawnPoint(respawnPoint);
+
         bossFightCamera.gameObject.SetActive(true);
         defaultCamera.gameObject.SetActive(false);
+        bossScript.enabled = true;
+        StartCoroutine(CloseHorizontalDoor());
         Debug.Log("Start boss fight");
     }
 
     private void OnTriggerExit(Collider other) {
+        if (other.gameObject.GetComponent<PlayerScript>() == null) {
+            return;
+        }
+
         bossScript.enabled = false;
         defaultCamera.gameObject.SetActive(true);
         bossFightCamera.gameObject.SetActive(false);
+        horizontalDoor.transform.position = horizontalDoorInitialPosition;
+    }
+
+    private IEnumerator CloseHorizontalDoor() {
+        yield return new WaitForSeconds(1f);
+        var target = horizontalDoorInitialPosition.x + 5f;
+        while (horizontalDoor.transform.position.x < target) {
+            horizontalDoor.transform.position += Vector3.right * (5.0f * Time.deltaTime);
+            yield return null;
+        }
     }
 }
